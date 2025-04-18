0528a4508d13118752d6a86e661437652129e31f
Garset <pitufa247@gmail.com>
2025-04-18 16:45:42 +0000

    cambio de codigo js

--- src/js/app.js
+++ src/js/app.js
@@ -22,61 +22,71 @@ import "../style/index.css";
         city: null
     }
  */
-    function render(variables = {}) {
-      console.log("These are the current variables: ", variables);
-      
-      // Cover logic
-      let cover = variables.includeCover 
-        ? `<div class="cover"><img src="${variables.background || ''}" /></div>`
-        : "<div class='cover'></div>";
-    
-      // Name logic
-      const fullName = [variables.name, variables.lastName]
-        .filter(Boolean)
-        .join(' ')
-        .trim() || 'No name provided';
-    
-      // Role logic
-      const role = variables.role || 'No role specified';
-    
-      // Location logic
-      const location = [variables.city, variables.country]
-        .filter(Boolean)
-        .join(', ')
-        .trim() || 'No location specified';
-    
-      // Social media logic
-      const socialMedia = [
-        { platform: 'twitter', username: variables.twitter },
-        { platform: 'github', username: variables.github },
-        { platform: 'linkedin', username: variables.linkedin },
-        { platform: 'instagram', username: variables.instagram }
-      ];
-    
-      const socialMediaLinks = socialMedia
-        .filter(sm => sm.username)
-        .map(sm => `
-          <li>
-            <a href="https://${sm.platform}.com/${sm.username}" target="_blank">
-              <i class="fab fa-${sm.platform}"></i>
-            </a>
-          </li>
-        `).join('');
-    
-      // Social media container
-      const socialMediaContainer = socialMediaLinks 
-        ? `<ul class="${variables.socialMediaPosition || 'position-right'}">${socialMediaLinks}</ul>`
-        : '';
-    
-      // HTML output
-      document.querySelector("#widget_content").innerHTML = `
-        <div class="widget">
-          ${cover}
-          <img src="${variables.avatarURL || ''}" class="photo" />
-          <h1>${fullName}</h1>
-          <h2>${role}</h2>
-          <h3>${location}</h3>
-          ${socialMediaContainer}
+function render(variables = {}) {
+  console.log("These are the current variables: ", variables); // print on the console
+  // here we ask the logical questions to make decisions on how to build the html
+  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  // reset the website body with the new html output
+  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+            ${cover}
+          <img src="${variables.avatarURL}" class="photo" />
+          <h1>${variables.name}  ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.country} , ${variables.city}</h3>
+          
+          <ul class="socialMediaPosition" >
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          </ul>
         </div>
-      `;
-    }
+    `;
+}
+
+/**
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
+ */
+window.onload = function() {
+  window.variables = {
+    // if includeCover is true the algorithm should show the cover image
+    includeCover: true,
+    // this is the image's url that will be used as a background for the profile cover
+    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    // this is the url for the profile avatar
+    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    // social media bar position (left or right)
+    socialMediaPosition: "position-left",
+    // social media usernames
+    twitter: null,
+    github: null,
+    linkedin: null,
+    instagram: null,
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
+  };
+  render(window.variables); // render the card for the first time
+
+  document.querySelectorAll(".picker").forEach(function(elm) {
+    elm.addEventListener("change", function(e) {
+      // <- add a listener to every input
+      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      let values = {};
+      values[attribute] =
+        this.value == "" || this.value == "null"
+          ? null
+          : this.value == "true"
+          ? true
+          : this.value == "false"
+          ? false
+          : this.value;
+      render(Object.assign(window.variables, values)); // render again the card with new values
+    });
+  });
+};
