cf86edf68ac6eaab7edc9861005a0723edbd4bae
Elena <pitufa247@gmail.com>
2025-04-21 18:16:34 +0000

    agregue ternarias

--- src/js/app.js
+++ src/js/app.js
@@ -33,18 +33,19 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name}  ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.country} , ${variables.city}</h3>
-          
-          <ul class="socialMediaPosition">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+              <h1>${variables.name || "Lucy"} ${variables.lastName || "Boilett"}</h1>
+              <h2>${variables.role || "Web Developer"}</h2>
+              <h3>${variables.city || "Miami"}, ${variables.country || "USA"}</h3>
+              
+              <ul class=${variables.socialMediaPosition}>
+                <li><a href="https://twitter.com/${variables.twitter ? variables.twitter : "4geeksacademy"}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://github.com/${variables.github ? variables.github : "4geeksacademy"}" target="_blank"><i class="fab fa-github"></i></a></li>
+                <li><a href="https://linkedin.com/${variables.linkedin ? variables.linkedin : "school/4geeksacademy"}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://instagram.com/${variables.instagram ? variables.instagram : "4geeksacademy"}" target="_blank"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+        `;
+}
   const socialBar = document.querySelector(".socialMediaPosition");
   if (socialBar) {
     const position = variables.socialMediaPosition || "position-left";
